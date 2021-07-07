e56bc1cc523f9848a63829f22d1c5d8d9289e98c
Abhay Patil <71277980+Abhay5855@users.noreply.github.com>
2021-07-07 09:54:34 +0530

    create: new componet slotmachine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-
+import Slotmachine from "./components/Slotmachine";
 export default function App() {
   return <div className="App"></div>;
 }
--- src/components/Slotmachine.js
+++ src/components/Slotmachine.js
@@ -1,1 +1,3 @@
 const Slotmachine = () => {};
+
+export default Slotmachine;
